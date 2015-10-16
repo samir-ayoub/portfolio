7b69fdee3d3c6ae1e3075c844140b3e161e8a68d
Denis Tierno <de.tierno@gmail.com>
2015-10-16 14:24:26 -0300

    Fix variable problem and made code improvements
    Fix local variable assignment problem by adding it in the router of each
    page. Move from view assignment to router assignment to clean the views
    a little bit.
    
    Change code strutucture in favor of declarative variable names
    (currentPage instead tipo) and removed bilingual code (comum to shared).
    With this you codebase will have only one readable language.
    
    Also improve code beauty by inserting some spaces where they are needed,
    ex: currentPage=='contact' in worse than currentPage == 'contact'

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about/index');
+  res.render('about/index', { currentPage: 'about' });
 });
 
 module.exports = router;
